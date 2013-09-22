912d004ea482dce0a57c3832d7e23815b1d6ba5e
Rilindo Foster <rilindo.foster@searshc.com>
2013-09-21 20:17:02 -0500

    Cleaned out formatting in README file

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'rilindo.foster@monzell.com'
 license          'All rights reserved'
 description      'Installs/Configures google-authenticator'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends "openssh"
