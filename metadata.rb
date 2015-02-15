497560d6316d80bd91f80e5f411616f62d7ef4bf
Rilindo Foster <rilindo.foster@monzell.com>
2015-02-15 16:06:10 -0600

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,4 @@ maintainer_email 'rilindo.foster@monzell.com'
 license          'All rights reserved'
 description      'Installs/Configures google-authenticator'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
-{"fedora" => '17.0'}.each do |os,ver|
-  supports os, ">= #{ver}"
-  case os
-    when 'fedora'
-      depends 'yum'
-    when 'ubuntu'
-      depends 'apt'
-  end
-end
+version          '0.1.9'
