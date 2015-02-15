a16634c82027a7c7762c8b17f6831e9d94fc5a0a
Rilindo Foster <rilindo.foster@monzell.com>
2015-02-15 15:55:36 -0600

    Updated .spec files to support current ChefSpec release.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,8 @@
 require 'chefspec'
 
 describe 'google-authenticator::default' do
-  
-  let(:chef_run) do
-    runner = ChefSpec::ChefRunner.new('platform' => 'fedora', 'version'=> '17')
-    runner.converge('google-authenticator::default')
-  end
+
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
   
   it 'installs google-authenticator package' do
     expect(chef_run).to install_package 'google-authenticator'
--- spec/sshd_spec.rb
+++ spec/sshd_spec.rb
@@ -1,14 +1,11 @@
 require 'chefspec'
 
 describe 'google-authenticator::sshd' do
-  
-  let(:chef_run) do
-    runner = ChefSpec::ChefRunner.new('platform' => 'fedora', 'version'=> '17')
-    runner.converge('google-authenticator::sshd')
-  end
+
+  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
   
   it 'runs ruby block to updated sshd code' do
-    expect(chef_run).to execute_ruby_block 'edit-sshd-pamd'
+    expect(chef_run).to run_ruby_block 'edit-sshd-pamd'
   end
 end
